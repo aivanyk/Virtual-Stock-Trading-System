c6d4bbf0579bb51f82e0d42df3e092e829691f6d
hj <hj>
2023-12-07 14:50:58 -0500

    [issue #14] prototype for page development

---
+++ src/Model/OwnDatabase.java
@@ -0,0 +1,11 @@
+package com.stock_test.Model;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OwnDatabase {
+    public static List<OwnStock> getOwnStocks(int customerId) {
+        return null;
+    }
+}
---
+++ src/Model/OwnStock.java
@@ -0,0 +1,19 @@
+package com.stock_test.Model;
+
+public class OwnStock {
+    private Stock stock;
+    private int amount;
+    private double averagePrice;
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public double getAveragePrice() {
+        return averagePrice;
+    }
+}
